bc98c269bf9914cf1364e75d570f0894d0beaf04
HanHyunSoo <gustn4563@gmail.com>
2022-09-06 09:27:01 +0900

    Start Project

---
+++ src/test/java/com/onsuyum/OnsuyumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onsuyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsuyumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
