2303211c73edcbe5e00a47f29103866a895d6580
HanHyunSoo <gustn4563@gmail.com>
2022-09-10 17:01:24 +0900

    feat : Add Storage Service

---
+++ src/main/java/com/onsuyum/storage/domain/service/StorageService.java
@@ -0,0 +1,14 @@
+package com.onsuyum.storage.domain.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+public interface StorageService {
+    String upload(MultipartFile file);
+    void delete(String fileName);
+
+    default String createRandomFileName(String originalFileName) {
+        return UUID.randomUUID() + " " + originalFileName;
+    }
+}
