43a372db1648636a9560b76977df32a33b2b2f32
HanHyunSoo <gustn4563@gmail.com>
2022-09-15 20:41:38 +0900

    feat : Add DTO requests

---
+++ src/main/java/com/onsuyum/restaurant/dto/request/RestaurantRequest.java
@@ -0,0 +1,40 @@
+package com.onsuyum.restaurant.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+public class RestaurantRequest {
+    private Long id;
+    private String name;
+    private String phone;
+    private List<String> time;
+    private String summary;
+    private String location;
+    private Double longitude;
+    private Double latitude;
+    private MultipartFile outsideImage;
+    private MultipartFile insideImage;
+    private Set<String> categoryNames;
+
+    @Builder
+    public RestaurantRequest(Long id, String name, String phone, List<String> time, String summary, String location, Double longitude, Double latitude, MultipartFile outsideImage, MultipartFile insideImage, Set<String> categoryNames) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.time = time;
+        this.summary = summary;
+        this.location = location;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.outsideImage = outsideImage;
+        this.insideImage = insideImage;
+        this.categoryNames = categoryNames;
+    }
+}
