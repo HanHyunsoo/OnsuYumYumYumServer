008f5a33c4c3a3c5ce9c79264c7a95a78c706179
HanHyunSoo <gustn4563@gmail.com>
2022-09-25 02:11:17 +0900

    feat : Add Tags and TypeResolvers to Swagger Configuration

--- src/main/java/com/onsuyum/config/SwaggerConfig.java
+++ src/main/java/com/onsuyum/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.onsuyum.config;
 
 import com.fasterxml.classmate.TypeResolver;
+import com.onsuyum.common.response.FailureResponseBody;
 import com.onsuyum.common.response.SuccessResponseBody;
+import com.onsuyum.restaurant.dto.request.MenuRequestForm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,6 +11,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -32,7 +35,9 @@ public class SwaggerConfig {
     public Docket swaggerApi(TypeResolver typeResolver) {
         return new Docket(DocumentationType.OAS_30)
                 .additionalModels(
-                        typeResolver.resolve(SuccessResponseBody.class)
+                        typeResolver.resolve(SuccessResponseBody.class),
+                        typeResolver.resolve(FailureResponseBody.class),
+                        typeResolver.resolve(MenuRequestForm.class)
                 )
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes())
@@ -40,7 +45,14 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.onsuyum"))
                 .paths(PathSelectors.any())
                 .build()
-                .useDefaultResponseMessages(false);
+                .useDefaultResponseMessages(false)
+                .tags(
+                        new Tag("Category API", "카테고리에 관련한 기능들을 처리"),
+                        new Tag("Menu API", "메뉴에 관련한 기능"),
+                        new Tag("Restaurant Category API", "음식점과 카테고리의 관계에 대한 기능들"),
+                        new Tag("Restaurant API", "음식점 관련 기능"),
+                        new Tag("Image API", "이미지 관련 기능")
+                );
     }
 
     private Set<String> getConsumeContentTypes() {
