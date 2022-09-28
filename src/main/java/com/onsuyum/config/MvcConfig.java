1a81891ef60c25427a6e2871e7756f9ce60ba46e
HanHyunSoo <gustn4563@gmail.com>
2022-09-28 09:44:58 +0900

    feat : Add CORS configuration

--- src/main/java/com/onsuyum/config/MvcConfig.java
+++ src/main/java/com/onsuyum/config/MvcConfig.java
@@ -2,10 +2,12 @@ package com.onsuyum.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +19,9 @@ import java.util.List;
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    String[] corsOrigins;
+
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -25,5 +30,13 @@ public class MvcConfig implements WebMvcConfigurer {
                 .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(formatter))
                 .build();
         converters.add(0, new MappingJackson2HttpMessageConverter(objectMapper));
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(corsOrigins)
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3000);
     }
 }
