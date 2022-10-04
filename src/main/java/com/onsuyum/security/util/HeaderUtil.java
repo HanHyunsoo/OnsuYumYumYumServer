13800b1b4e3f56463e0ef9a435936482fad4bd7d
HanHyunSoo <gustn4563@gmail.com>
2022-10-04 15:54:05 +0900

    feat : Add Header Util

---
+++ src/main/java/com/onsuyum/security/util/HeaderUtil.java
@@ -0,0 +1,28 @@
+package com.onsuyum.security.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HeaderUtil {
+
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer ";
+
+    /**
+     * header에 존재하는 jwt token을 반환
+     * @param request request
+     * @return jwt token or null
+     */
+    public static String getAccessToken(HttpServletRequest request) {
+        String headerValue = request.getHeader(HEADER_AUTHORIZATION);
+
+        if (headerValue == null) {
+            return null;
+        }
+
+        if (headerValue.startsWith(TOKEN_PREFIX)) {
+            return headerValue.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
+    }
+}
