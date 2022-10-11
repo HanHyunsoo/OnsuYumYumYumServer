4a7b5b2381ec00bd856387d560ef22e4aa693200
HanHyunSoo <gustn4563@gmail.com>
2022-10-11 11:32:00 +0900

    feat : Edit Security Configuration

--- src/main/java/com/onsuyum/config/SecurityConfig.java
+++ src/main/java/com/onsuyum/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.onsuyum.config;
 
 import com.onsuyum.security.filter.JwtAuthenticationFilter;
+import com.onsuyum.security.handler.RestAuthenticationEntryPoint;
 import com.onsuyum.security.token.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,9 +25,15 @@ public class SecurityConfig {
                 .csrf().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new RestAuthenticationEntryPoint(jwtTokenProvider))
+                .and()
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/api/auth/test").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/auth").authenticated()
                 .antMatchers("/**").permitAll()
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
