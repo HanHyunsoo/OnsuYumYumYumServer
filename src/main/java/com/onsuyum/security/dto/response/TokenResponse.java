fef797fab397a643421fbc5a5956af48e9548819
HanHyunSoo <gustn4563@gmail.com>
2022-10-11 11:30:43 +0900

    feat : Add DTOs for Security

--- src/main/java/com/onsuyum/security/dto/response/TokenResponse.java
+++ src/main/java/com/onsuyum/security/dto/response/TokenResponse.java
@@ -1,13 +1,16 @@
 package com.onsuyum.security.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "토큰 Response")
 public class TokenResponse {
 
+    @Schema(description = "jwt 엑세스 토큰")
     private String accessToken;
 
     @Builder
--- src/main/java/com/onsuyum/security/dto/response/UserResponse.java
+++ src/main/java/com/onsuyum/security/dto/response/UserResponse.java
@@ -1,5 +1,6 @@
 package com.onsuyum.security.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,11 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "유저 정보 Response")
 public class UserResponse {
+    @Schema(description = "현재 로그인 된 유저 아이디", example = "gustn8523")
     private String username;
+    @Schema(description = "현재 로그인 한 유저의 roles")
     private List<String> roles;
 
     @Builder
