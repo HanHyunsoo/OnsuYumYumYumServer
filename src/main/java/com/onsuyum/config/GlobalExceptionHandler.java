e0228fd3d8b5916e7a528079392bdb880a068e57
HanHyunSoo <gustn4563@gmail.com>
2022-10-19 10:53:19 +0900

    refactor : Reduce Code in GlobalExceptionHandler

--- src/main/java/com/onsuyum/config/GlobalExceptionHandler.java
+++ src/main/java/com/onsuyum/config/GlobalExceptionHandler.java
@@ -9,73 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(basePackages = "com.onsuyum")
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BabfulMenuDateAlreadyExistsException.class)
-    public ResponseEntity<FailureResponseBody> handleBabfulMenuDateAlreadyExistsException(BabfulMenuDateAlreadyExistsException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(BabfulMenuNotFoundException.class)
-    public ResponseEntity<FailureResponseBody> handleBabfulMenuNotFoundException(BabfulMenuNotFoundException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(CategoryNotFoundException.class)
-    public ResponseEntity<FailureResponseBody> handleCategoryNotFoundException(CategoryNotFoundException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(CouldNotLoadImageFileException.class)
-    public ResponseEntity<FailureResponseBody> handleCouldNotLoadImageFileException(CouldNotLoadImageFileException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(CouldNotSaveFileInLocal.class)
-    public ResponseEntity<FailureResponseBody> handleCouldNotSaveFileInLocal(CouldNotSaveFileInLocal e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(CouldNotSaveFileInS3.class)
-    public ResponseEntity<FailureResponseBody> handleCouldNotSaveFileInS3(CouldNotSaveFileInS3 e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(ForbiddenRestaurantException.class)
-    public ResponseEntity<FailureResponseBody> handleForbiddenRestaurantException(ForbiddenRestaurantException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(ImageNotFoundException.class)
-    public ResponseEntity<FailureResponseBody> handleImageNotFoundException(ImageNotFoundException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(LocalFileNotFoundException.class)
-    public ResponseEntity<FailureResponseBody> handleLocalFileNotFoundException(LocalFileNotFoundException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(LoginFailException.class)
-    public ResponseEntity<FailureResponseBody> handleLoginFailException(LoginFailException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(MenuNotFoundException.class)
-    public ResponseEntity<FailureResponseBody> handleMenuNotFoundException(MenuNotFoundException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(RestaurantNotFoundException.class)
-    public ResponseEntity<FailureResponseBody> handleRestaurantNotFoundException(RestaurantNotFoundException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(RestaurantTimeNotValidException.class)
-    public ResponseEntity<FailureResponseBody> handleRestaurantTimeNotValidException(RestaurantTimeNotValidException e) {
-        return FailureResponseBody.toResponseEntity(e.getStatusEnum());
-    }
-
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<FailureResponseBody> handleUserNotFoundException(UserNotFoundException e) {
+    @ExceptionHandler(CustomAbstractException.class)
+    public ResponseEntity<FailureResponseBody> handelExceptions(CustomAbstractException e) {
         return FailureResponseBody.toResponseEntity(e.getStatusEnum());
     }
 }
