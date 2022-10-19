47d3955e940aff8d4b3b566aaca726d2790998f4
HanHyunSoo <gustn4563@gmail.com>
2022-10-19 10:38:23 +0900

    feat : Add Resource Handlers and Date Serializer

--- src/main/java/com/onsuyum/config/MvcConfig.java
+++ src/main/java/com/onsuyum/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.onsuyum.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
@@ -10,11 +11,14 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -34,11 +38,18 @@ public class MvcConfig implements WebMvcConfigurer {
                 .serializersByType(
                         Map.of(
                                 LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter),
-                                LocalDate.class, new LocalDateSerializer(dateFormatter)
+                                LocalDate.class, new LocalDateSerializer(dateFormatter),
+                                Date.class, new DateSerializer(false, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
                         )
                 )
                 .deserializerByType(LocalDate.class, new LocalDateDeserializer(dateFormatter))
                 .build();
         converters.add(0, new MappingJackson2HttpMessageConverter(objectMapper));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
