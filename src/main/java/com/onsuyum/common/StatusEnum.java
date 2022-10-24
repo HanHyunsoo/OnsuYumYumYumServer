1b79b9d504a4b48c980d15c108bf3c0dc63f6094
HanHyunSoo <gustn4563@gmail.com>
2022-10-24 21:04:28 +0900

    feat : Add Custom Exception for Menu

--- src/main/java/com/onsuyum/common/StatusEnum.java
+++ src/main/java/com/onsuyum/common/StatusEnum.java
@@ -12,11 +12,16 @@ public enum StatusEnum {
     SUCCESS_GET_CATEGORIES(OK, "카테고리들 불러오기 성공"),
     SUCCESS_GET_CATEGORY(OK, "카테고리 불러오기 성공"),
     SUCCESS_GET_MENUS(OK, "메뉴들 불러오기 성공"),
+    SUCCESS_GET_MENU(OK, "메뉴 불러오기 성공"),
     SUCCESS_GET_RESTAURANTS(OK, "음식점들 불러오기 성공"),
     SUCCESS_GET_RESTAURANT(OK, "음식점 불러오기 성공"),
     SUCCESS_GET_RANDOM_RESTAURANT(OK, "임의의 음식점 불러오기 성공"),
     SUCCESS_LOGOUT(OK, "로그아웃 성공(액세스 토큰 제거)"),
     SUCCESS_GET_USER_INFO(OK, "현재 로그인 된 유저의 정보 불러오기 성공"),
+    SUCCESS_UPDATE_MENU(OK, "메뉴 업데이트 성공"),
+    SUCCESS_DELETE_MENU(OK, "메뉴 삭제 성공"),
+    SUCCESS_UPDATE_MENU_IMAGE(OK, "메뉴 이미지 업데이트 성공"),
+    SUCCESS_DELETE_MENU_IMAGE(OK, "메뉴 이미지 삭제 성공"),
 
     // 201
     SUCCESS_CREATE_MENUS(CREATED, "메뉴들 정보 저장 성공"),
@@ -32,6 +37,7 @@ public enum StatusEnum {
     // 400
     NEED_CATEGORY_VALUE(BAD_REQUEST, "카테고리를 한개 이상 요청 보내야 합니다."),
     BABFUL_MENU_DATE_ALREADY_EXISTS(BAD_REQUEST, "이미 해당 날짜에 밥풀 식단 데이터가 존재합니다."),
+    MENU_IMAGE_ALREADY_EXISTS(BAD_REQUEST, "해당 메뉴에 이미지가 존재합니다."),
 
     // 401
     LOGIN_FAIL(UNAUTHORIZED, "아이디 또는 비밀번호가 잘못되어 로그인을 할 수 없습니다."),
@@ -51,6 +57,7 @@ public enum StatusEnum {
     BABFUL_MENU_NOT_FOUND(NOT_FOUND, "해당 ID를 가진 밥풀 식단 없음"),
     LOCAL_FILE_NOT_FOUND(NOT_FOUND, "로컬 저장소에 파일이 존재하지 않음"),
     USER_NOT_FOUND(NOT_FOUND, "유저를 찾을 수 없음"),
+    MENU_IMAGE_NOT_FOUND(BAD_REQUEST, "해당 메뉴에 이미지가 존재하지 않습니다."),
 
     // 500
     COULD_NOT_SAVE_FILE_IN_LOCAL(INTERNAL_SERVER_ERROR, "로컬 저장소에 파일을 저장 할 수 없습니다"),
