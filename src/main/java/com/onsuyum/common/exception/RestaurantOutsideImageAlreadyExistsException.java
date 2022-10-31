2953f3df8c9ac5be0b0e5d27ac158b1c886ba443
HanHyunSoo <gustn4563@gmail.com>
2022-10-31 15:28:41 +0900

    feat : Add Custom Exceptions for Admin Restaurant

---
+++ src/main/java/com/onsuyum/common/exception/RestaurantOutsideImageAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.onsuyum.common.exception;
+
+import com.onsuyum.common.StatusEnum;
+
+public class RestaurantOutsideImageAlreadyExistsException extends CustomAbstractException {
+    public RestaurantOutsideImageAlreadyExistsException() {
+        super(StatusEnum.RESTAURANT_OUTSIDE_IMAGE_ALREADY_EXISTS);
+    }
+}
