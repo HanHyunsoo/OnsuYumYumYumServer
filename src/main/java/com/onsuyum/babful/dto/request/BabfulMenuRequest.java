69d6add915b44c234a5eb57f070a0e713ff0bc06
HanHyunSoo <gustn4563@gmail.com>
2022-11-11 11:21:00 +0900

    fix : Add NoArgConstructor in Babful dto

--- src/main/java/com/onsuyum/babful/dto/request/BabfulMenuRequest.java
+++ src/main/java/com/onsuyum/babful/dto/request/BabfulMenuRequest.java
@@ -5,19 +5,21 @@ import java.time.LocalDate;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Schema(description = "밥풀 식단 Request")
+@NoArgsConstructor
 public class BabfulMenuRequest {
 
-    @Schema(description = "밥풀 식단 날짜", example = "2022-10-11")
+    @Schema(description = "밥풀 식단 날짜", example = "2022-10-11", implementation = String.class)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private final LocalDate menuDate;
-    @Schema(description = "밥풀 식단 각 음식들", example = "[ \"밥\", \"국\", \"김치\"]")
-    private final List<String> foods;
+    private LocalDate menuDate;
+    @Schema(description = "밥풀 식단 각 음식들")
+    private List<String> foods;
     @Schema(description = "밥풀 식단 중 맛있는 음식", example = "김치")
-    private final String deliciousFood;
+    private String deliciousFood;
 
     @Builder
     public BabfulMenuRequest(LocalDate menuDate, List<String> foods, String deliciousFood) {
